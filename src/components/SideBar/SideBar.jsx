1d1a2a01c135cc8e0ee74729ce1aa0fe2228f1a9
wingskh <deathlwing@gmail.com>
2022-08-26 12:19:08 +0800

    updated sidebar listitem ui

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./sideBar.scss";
 import { Link } from "react-router-dom";
 import {
@@ -31,8 +32,11 @@ const sideBarItemList = [
 
 export const SideBar = () => {
   const dispatch = useDispatch();
-  const pageClickHandler = (page) => {
-    dispatch(changePageActionCreator(page));
+  const [selectedPage, setSelectedPage] = useState("About");
+
+  const handleSideBarItemClick = (itemName) => {
+    dispatch(changePageActionCreator(itemName));
+    setSelectedPage(itemName);
   };
 
   const tempComponent = (
@@ -53,7 +57,7 @@ export const SideBar = () => {
                   to={item.path}
                   key={item.text}
                   className="sideBarItem"
-                  onClick={() => pageClickHandler(item.text)}
+                  onClick={() => handleSideBarItemClick(item.text)}
                 >
                   <div className="sideBarItemIcon">{item.icon}</div>
                   <Box
@@ -70,48 +74,148 @@ export const SideBar = () => {
       </div>
     </div>
   );
-  const drawerWidth = 280;
-
-  return (
-    <Drawer
-      sx={{
-        width: drawerWidth,
-        flexShrink: 0,
-        "& .MuiDrawer-paper": {
-          width: drawerWidth,
-          boxSizing: "border-box",
+  const drawerWidth = 260;
+  const theme = createTheme({
+    components: {
+      MuiListItem: {
+        styleOverrides: {
+          root: {
+            "&.Mui-selected": {
+              backgroundColor: "red",
+            },
+          },
         },
-      }}
-      variant="permanent"
-      anchor="left"
-      className="sidebarContainer"
-    >
-      <div className="background">
-        <Typography variant="h5" component="div" className="headerText">
-          Wing
-        </Typography>
-        <Divider className="topDivider" />
-        <div className="backgroundFilter" />
-        <List className="sideBarItemContainer">
-          {sideBarItemList.map((item) => (
-            <ListItem key={item.text} disablePadding>
-              <Link
-                to={item.path}
+      },
+    },
+  });
+  const hexToRgb = (input) => {
+    input = input + "";
+    input = input.replace("#", "");
+    let hexRegex = /[0-9A-Fa-f]/g;
+    if (!hexRegex.test(input) || (input.length !== 3 && input.length !== 6)) {
+      throw new Error("input is not a valid hex color.");
+    }
+    if (input.length === 3) {
+      let first = input[0];
+      let second = input[1];
+      let last = input[2];
+      input = first + first + second + second + last + last;
+    }
+    input = input.toUpperCase();
+    let first = input[0] + input[1];
+    let second = input[2] + input[3];
+    let last = input[4] + input[5];
+    return (
+      parseInt(first, 16) +
+      ", " +
+      parseInt(second, 16) +
+      ", " +
+      parseInt(last, 16)
+    );
+  };
+  return (
+    <ThemeProvider theme={theme}>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          // maxWidth: drawerWidth,
+          // width: "100%",
+          flexShrink: 5,
+          // position: "sticky",
+          "& .MuiDrawer-paper": {
+            width: drawerWidth,
+            // width: "100%",
+            // maxWidth: drawerWidth,
+            boxSizing: "border-box",
+          },
+        }}
+        variant="permanent"
+        anchor="left"
+        className="sidebarContainer"
+      >
+        <div className="background">
+          <Typography variant="h5" component="div" className="headerText">
+            Wing
+          </Typography>
+          <Divider className="topDivider" />
+          <div className="backgroundFilter" />
+          <List className="sideBarItemContainer">
+            {sideBarItemList.map((item) => (
+              <ListItem
+                disablePadding
                 key={item.text}
-                className="sideBarItem"
-                onClick={() => pageClickHandler(item.text)}
+                sx={{ margin: "10px 15px 0", width: "auto" }}
               >
-                <ListItemButton>
-                  <ListItemIcon className="sideBarItemIcon">
-                    {item.icon}
-                  </ListItemIcon>
-                  <ListItemText primary={item.text} />
-                </ListItemButton>
-              </Link>
-            </ListItem>
-          ))}
-        </List>
-      </div>
-    </Drawer>
+                <Link
+                  to={item.path}
+                  key={item.text}
+                  className="sideBarItem"
+                  onClick={() => handleSideBarItemClick(item.text)}
+                >
+                  <ListItemButton
+                    sx={{
+                      borderRadius: "3px",
+                      "&.Mui-selected": {
+                        backgroundColor: "#00acc1",
+                        boxShadow:
+                          "0 12px 20px -10px rgba(" +
+                          hexToRgb("#00acc1") +
+                          ",.28), 0 4px 20px 0 rgba(" +
+                          hexToRgb("#000") +
+                          ",.12), 0 7px 8px -5px rgba(" +
+                          hexToRgb("#00acc1") +
+                          ",.2)",
+                        "&:hover,&:focus": {
+                          backgroundColor: "#00acc1",
+                          boxShadow:
+                            "0 12px 20px -10px rgba(" +
+                            hexToRgb("#00acc1") +
+                            ",.28), 0 4px 20px 0 rgba(" +
+                            hexToRgb("#000") +
+                            ",.12), 0 7px 8px -5px rgba(" +
+                            hexToRgb("#00acc1") +
+                            ",.2)",
+                        },
+                      },
+                      "&.Mui-focusVisible": {
+                        backgroundColor: "#00acc1",
+                        boxShadow:
+                          "0 12px 20px -10px rgba(" +
+                          hexToRgb("#00acc1") +
+                          ",.28), 0 4px 20px 0 rgba(" +
+                          hexToRgb("#000") +
+                          ",.12), 0 7px 8px -5px rgba(" +
+                          hexToRgb("#00acc1") +
+                          ",.2)",
+                        "&:hover,&:focus": {
+                          backgroundColor: "#00acc1",
+                          boxShadow:
+                            "0 12px 20px -10px rgba(" +
+                            hexToRgb("#00acc1") +
+                            ",.28), 0 4px 20px 0 rgba(" +
+                            hexToRgb("#000") +
+                            ",.12), 0 7px 8px -5px rgba(" +
+                            hexToRgb("#00acc1") +
+                            ",.2)",
+                        },
+                      },
+                      // // hover style
+                      // ":hover": {
+                      // },
+                    }}
+                    selected={selectedPage === item.text}
+                  >
+                    <ListItemIcon className="sideBarItemIcon">
+                      {item.icon}
+                    </ListItemIcon>
+                    <ListItemText primary={item.text} />
+                  </ListItemButton>
+                </Link>
+              </ListItem>
+            ))}
+          </List>
+        </div>
+      </Drawer>
+    </ThemeProvider>
   );
 };
